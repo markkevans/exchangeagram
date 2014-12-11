3d72fc51a7c177fae8aa6975a382b9d0a4e1ce01
Mark Evans <mark@motilo.com>
2014-12-11 17:25:12 +0000

    Setup custom feed cell class

---
+++ ExchangeAGram/FeedCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedCell.swift
+//  ExchangeAGram
+//
+//  Created by Mark Evans on 11/12/2014.
+//  Copyright (c) 2014 Mark Evans. All rights reserved.
+//
+
+import UIKit
+
+class FeedCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var captionLabel: UILabel!
+}
