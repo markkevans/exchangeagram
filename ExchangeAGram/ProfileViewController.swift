e100e22ab19fd22464c4d5c28a05623f18f39982
Mark Evans <markkevans@gmail.com>
2014-12-13 17:49:45 +0000

    Add Facebook login functionality

--- ExchangeAGram/ProfileViewController.swift
+++ ExchangeAGram/ProfileViewController.swift
@@ -8,15 +8,21 @@
 
 import UIKit
 
-class ProfileViewController: UIViewController {
+class ProfileViewController: UIViewController, FBLoginViewDelegate {
 
     @IBOutlet weak var profileImageView: UIImageView!
     @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var fbLoginView: FBLoginView!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        self.fbLoginView.delegate = self
+        self.fbLoginView.readPermissions = ["public_profile", "publish_actions"]
     }
 
     override func didReceiveMemoryWarning() {
@@ -24,15 +30,27 @@ class ProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func loginViewShowingLoggedInUser(loginView: FBLoginView!) {
+        self.profileImageView.hidden = false
+        self.nameLabel.hidden = false
     }
-    */
-
+    
+    func loginViewFetchedUserInfo(loginView: FBLoginView!, user: FBGraphUser!) {
+        println(user)
+        self.nameLabel.text = user.name
+        let userImageUrl = "https://graph.facebook.com/\(user.objectID)/picture?type=small"
+        let url = NSURL(string: userImageUrl)
+        let imageData = NSData(contentsOfURL: url!)
+        let image = UIImage(data: imageData!)
+        self.profileImageView.image = image
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView: FBLoginView!) {
+        self.profileImageView.hidden = true
+        self.nameLabel.hidden = true
+    }
+    
+    func loginView(loginView: FBLoginView!, handleError error: NSError!) {
+        println("Error: \(error.localizedDescription)")
+    }
 }
