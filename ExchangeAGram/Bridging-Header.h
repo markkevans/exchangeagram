bc74cd73352d40cd1a09d59ae3c233b9752e52e4
Mark Evans <markkevans@gmail.com>
2014-12-13 07:58:29 +0000

    Add logic for Facebook SDK

---
+++ ExchangeAGram/Bridging-Header.h
@@ -0,0 +1,10 @@
+//
+//  Bridging-Header.h
+//  ExchangeAGram
+//
+//  Created by Mark Evans on 13/12/2014.
+//  Copyright (c) 2014 Mark Evans. All rights reserved.
+//
+
+
+#import <FacebookSDK/FacebookSDK.h>
